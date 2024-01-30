a9ed6b17fcebb915a6ccd0de1326e6cb017a84b6
koize <justinweng6@gmail.com>
2024-01-30 11:03:51 +0800

    add payment code

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from hal import hal_led as led
 from hal import hal_lcd as LCD
 from hal import hal_keypad as keypad
 from hal import hal_buzzer as buzzer
+from hal import hal_rfid_reader as rfid_reader
+import cam
 import time
 
 def display_main_menu():
@@ -39,7 +41,7 @@ def menu_selection(option):
         lcd.lcd_backlight(0)
 
 def camera_scanning():
-    #code for camera
+    data = cam.scan_qr()
     
 
 def buzzer_scanning():
@@ -65,22 +67,83 @@ def display_payment_screen(total_price):
 
         key = keypad.get_key()
         if key == "1":
-            # Process to payment
+            display_payment_method_menu()
             break
         elif key == "2":
             # Process to camera scanning
             break
         
+
 def display_payment_method_menu():
     lcd = LCD.lcd()
-
+    key = keypad.get_key()
+    
     # Clear the LCD
     lcd.lcd_clear()
 
     # Display the payment method menu
     lcd.lcd_display_string("Payment Method?", 1)
     lcd.lcd_display_string("1. ATM, 2. PayWave", 2)
+
+    if key == "1":
+        pay_with_atm()
+    elif key == "2":
+        pay_with_paywave()
+    else:
+        lcd.lcd_display_string("Invalid number", 2)
+        time.sleep(2)
+        display_payment_method_menu()
+
+    time.sleep(2)
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Thank you", 1)
+    lcd.lcd_display_string("Come again!", 2)
+    display_main_menu()
+
+
+
+def pay_with_atm():
+    key = keypad.get_key()
+    lcd = LCD.lcd()
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Enter your PIN", 1)
+    lcd.lcd_display_string("Press # to confirm", 2)
+    if key in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        lcd.lcd_display_string("*", 2)
+    elif key == "#":
+        buzzer.beep(0.1, 0, 1)
+        verify_pin()
+        break
+
+
+def verify_pin():
+    lcd = LCD.lcd()
+    lcd.lcd_clear()
+    time.sleep(2)
+    if key == "1234":
+        lcd.lcd_display_string("PIN verified", 1)
+    elif key != "1234":
+        lcd.lcd_display_string("Invalid PIN", 1)
+        time.sleep(2)
+        pay_with_atm()
+
+
+def pay_with_paywave():
+    key = keypad.get_key()
+    lcd = LCD.lcd()
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Tap your card", 1)
+    reader = rfid_reader.init()
+    id = reader.read_id_no_block()
+            id = str(id)
         
+            if id != "None":
+                buzzer.beep(1, 0, 1)
+                print("RFID card ID = " + id)
+                # Display RFID card ID on LCD line 2
+                lcd.lcd_display_string(id, 2) 
+            time.sleep(2)   
+
 
 def main():
     #Initiallize LED driver
